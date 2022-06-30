1a3a3bab37b075a7b9ccc52ff301d88fe1d8cdfe
JHONNATHAN RICARDO ALBARRACIN PERALTA <jhonnathanricardoalbarracinperalta@JHONNATHANs-MacBook-Pro.local>
2022-06-30 15:45:25 -0500

    form cuentas

--- src/app/verificar/pages/trabajador/trabajador.component.ts
+++ src/app/verificar/pages/trabajador/trabajador.component.ts
@@ -242,4 +242,97 @@ export class TrabajadorComponent implements OnInit {
   itemCheckBox(item): AppInput {
     return new AppInput({ type: 'text' });
   }
+  checkChange(event, item) {
+    console.log(event, item);
+    if (!event) return;
+    const form: AppFormGeneric = new AppFormGeneric(
+      new AppFormGeneric({
+        controls: [
+          {
+            type: 'select',
+            validators: [Validators.required],
+            formControlName: 'tipo',
+            class: 'col-12 col-md-6',
+            label: 'Tipo de cuenta',
+            options: [
+              {
+                label: 'Cuenta Corriente',
+                value: faker.datatype.number({ min: 10000 }),
+              },
+              {
+                label: 'Ahorro Plazo',
+                value: faker.datatype.number({ min: 10000 }),
+              },
+              {
+                label: 'Cuenta de Ahorro',
+                value: faker.datatype.number({ min: 10000 }),
+              },
+            ],
+          },
+          {
+            type: 'select',
+            validators: [Validators.required],
+            formControlName: 'banco',
+            class: 'col-12 col-md-6',
+            label: 'Banco',
+            options: [
+              {
+                label: 'BANCO DE CHILE',
+                value: faker.datatype.number({ min: 10000 }),
+              },
+              {
+                label: 'BANCO INTERNACIONAL',
+                value: faker.datatype.number({ min: 10000 }),
+              },
+              {
+                label: 'SCOTIABANK',
+                value: faker.datatype.number({ min: 10000 }),
+              },
+            ],
+          },
+          {
+            type: 'number',
+            validators: [Validators.required],
+            formControlName: 'numero',
+            class: 'col-12 col-md-6',
+            label: 'Numero de cuenta',
+            value: faker.datatype.number({ min: 100000 }),
+          },
+        ],
+        updateOn: 'change',
+        class: 'p-1',
+        clean: new AppFormButton({
+          show: true,
+          type: 'button',
+          class: 'btn',
+          color: '',
+          framework: 'material',
+          label: 'Cancelar',
+        }),
+        submit: new AppFormButton({
+          show: true,
+          type: 'submit',
+          class: 'btn',
+          color: 'primary',
+          framework: 'material',
+          label: 'Guardar',
+        }),
+      })
+    );
+    const modald = this.modalService
+      .new(
+        new AppModal({
+          title: 'Agregar cuenta',
+          data: form,
+          id: 'newItem',
+          component: ModalFormComponent,
+        })
+      )
+      .open()
+      .closeEvent()
+      .subscribe((data) => {
+        console.log(data);
+      });
+  
+  }
 }
